bd4536f799eb01925850ec98cb74b4270d6328c1
Sujith N P <Sujith.Np@Ltts.com>
2025-11-17 14:13:53 +0530

    clear main branch

--- EmployeeDetails/Models/Department.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EmployeeDetails.Models
-{
-    public class Department
-    {
-        public int Id { get; set; }
-        public string DeptName { get; set; }
-        public ICollection<Employee> Employees { get; set; }
-    }
-}
